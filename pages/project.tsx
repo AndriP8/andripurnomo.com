fae1f5e1f39a2eda70cc5a629802d0a4b588818d
AndriP8 <andrikcoc26@gmail.com>
2022-01-09 21:18:27 +0700

    add route for each project

--- pages/project.tsx
+++ pages/project.tsx
@@ -3,26 +3,16 @@ import Layout from "layout";
 const project = () => {
   return (
     <Layout>
-      <div className="h-auto w-full mt-28 px-4 sm:px-8 md:px-52">
-        <div className="grid grid-cols-2 auto-rows-auto">
+      <div className="h-auto w-full mt-28 width-container">
+        <div className="grid grid-cols-1 lg:grid-cols-2 auto-rows-auto pt-8 pb-16">
           <div className="border-2 rounded-lg py-8 px-8 m-2 border-gray-400">
-            <h2 className="font-semibold">Movie App</h2>
-            <p className="pt-2 pb-4">
-              A web that displays a list of trending movies and a description of
-              the film that consume data provided by TMDB{" "}
-            </p>
-            <div className="flex items-center justify-start flex-wrap">
-              <p className="p-2 mr-4 border-2 rounded-xl border-black bg-gray-500">
-                React js
-              </p>
-              <p className="p-2 mr-4 border-2 rounded-xl border-black bg-gray-500">
-                TailwindCSS
-              </p>
-            </div>
-          </div>
-          <div className="border-2 rounded-lg py-8 px-8 m-2 border-gray-400">
-            <h2 className="font-semibold">Cryptocurrency coin</h2>
-            <p className="pt-2 pb-4">
+            <a
+              href="https://gecko-coin.vercel.app/"
+              className="font-semibold text-3xl"
+            >
+              Cryptocurrency coin
+            </a>
+            <p className="py-4 text-lg">
               A web that is a clone of{" "}
               <a href="https://www.coingecko.com/" className="underline">
                 coingecko
@@ -35,6 +25,9 @@ const project = () => {
                 React js
               </p>
               <p className="p-2 mr-4 my-1 border-2 rounded-xl border-black bg-gray-500">
+                Typescript
+              </p>
+              <p className="p-2 mr-4 my-1 border-2 rounded-xl border-black bg-gray-500">
                 TailwindCSS
               </p>
               <p className="p-2 mr-4 my-1 border-2 rounded-xl border-black bg-gray-500">
@@ -43,8 +36,65 @@ const project = () => {
             </div>
           </div>
           <div className="border-2 rounded-lg py-8 px-8 m-2 border-gray-400">
-            <h2 className="font-semibold">Quote random generator</h2>
-            <p className="pt-2 pb-4">
+            <a
+              href="https://tmdb-movie-three.vercel.app/"
+              className="font-semibold text-3xl"
+            >
+              Movie App
+            </a>
+            <p className="py-4 text-lg">
+              A web that displays a list of trending movies and a description of
+              the film that consume data provided by TMDB{" "}
+            </p>
+            <p className="mb-4 text-sm underline">
+              !!! The search feature is having problems because the api source
+              is still using http, it is recommended to clone from the{" "}
+              <a
+                href="https://github.com/AndriP8/tmdb_movie"
+                className="font-bold"
+              >
+                source code
+              </a>
+            </p>
+            <div className="flex items-center justify-start flex-wrap">
+              <p className="p-2 mr-4 border-2 rounded-xl border-black bg-gray-500">
+                React js
+              </p>
+              <p className="p-2 mr-4 border-2 rounded-xl border-black bg-gray-500">
+                TailwindCSS
+              </p>
+            </div>
+          </div>
+          <div className="border-2 rounded-lg py-8 px-8 m-2 border-gray-400">
+            <a
+              href="https://game-store-tau.vercel.app/"
+              className="font-semibold text-3xl"
+            >
+              Game store App
+            </a>
+            <p className="py-4 text-lg">
+              A website that provides a way to top up coins for various games
+            </p>
+            <div className="flex items-center justify-start flex-wrap">
+              <p className="p-2 mr-4 border-2 rounded-xl border-black bg-gray-500">
+                Typescript
+              </p>
+              <p className="p-2 mr-4 border-2 rounded-xl border-black bg-gray-500">
+                Next JS
+              </p>
+              <p className="p-2 mr-4 border-2 rounded-xl border-black bg-gray-500">
+                TailwindCSS
+              </p>
+            </div>
+          </div>
+          <div className="border-2 rounded-lg py-8 px-8 m-2 border-gray-400">
+            <a
+              href="https://andrip8.github.io/FCC-RandomQuoteMachine/"
+              className="font-semibold text-3xl"
+            >
+              Quote random generator
+            </a>
+            <p className="py-4 text-lg">
               A website that provides quotes that can be shared on twitter
             </p>
             <div className="flex items-center justify-start flex-wrap">
