aa24d757efeefc813e77320595e944c9a81f9d31
Andri Purnomo <andri.adrp@gmail.com>
2023-12-26 10:06:03 +0700

    feat: revamp blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,16 @@
+import { getBlogs } from '@lib/data';
+import { CardLinkBlog } from '@ui/components';
+
+export default async function Page() {
+  const blogs = await getBlogs();
+  return (
+    <div className="space-content my-28">
+      <h2 className="text-4xl my-4">List Blog</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4">
+        {blogs.map((blog) => (
+          <CardLinkBlog key={blog.slug} blog={blog} />
+        ))}
+      </div>
+    </div>
+  );
+}
