613eba73ef7976b3d3c7236b44668b8d65993734
Andri Purnomo <andri.adrp@gmail.com>
2023-12-28 10:33:31 +0700

    feat: fix summary about me at homepage

--- app/page.tsx
+++ app/page.tsx
@@ -19,13 +19,16 @@ export default async function Page() {
           <div className="prose max-w-none">
             <h1 className="text-4xl">Hi, Im Andri Purnomo</h1>
             <p className="text-lg">
-              Someone who is very interested in frontend development since late
-              2020. I am currently working as Frontend Engineer, my main tech
-              stack is React and Typescript, but I also learned other things
-              like Chakra UI, Mantine as a UI library, Xstate as a Finite state
-              machine, and Advanced React pattern. Now I am curious about how to
-              maintain an extensive website because for that I am interested
-              learn about performance
+              Passionate about frontend development since 2020, I&lsquo;m
+              currently working as a Frontend Engineer. I specialize in React,
+              Typescript, and NextJS. Going beyond the basics, I&lsquo;ve
+              explored Chakra UI and TailwindCSS for styling, Xstate as a Finite
+              State Machine, and delved into Advanced React patterns. Now, my
+              focus has shifted towards optimizing performance. It&lsquo;s not
+              just about code, it&lsquo;s ensuring fast loading, efficient
+              resource use, and a smoother user experience. Unraveling the
+              intricacies of maintaining an extensive website is my latest
+              exploration.
             </p>
           </div>
           <div>
