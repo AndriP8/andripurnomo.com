9fd53470296be7cf213188cb3b5b92e74f11440a
Andri Purnomo <andri.adrp@gmail.com>
2023-12-31 10:51:37 +0700

    fix: empty blog data when build

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,7 +5,6 @@ import { TwitterEmbed, YouTubeEmbed } from '@ui/components';
 import { Dots } from '@ui/components/Dots';
 import { Metadata } from 'next';
 import Image from 'next/image';
-
 type Props = {
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
@@ -19,13 +18,6 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-export async function generateStaticParams() {
-  const blogSlugs = await getBlogSlugs();
-  return blogSlugs.map((slug) => {
-    slug;
-  });
-}
-
 type PageProps = {
   params: {
     slug: string;
@@ -34,74 +26,82 @@ type PageProps = {
 
 export default async function Page({ params }: PageProps) {
   const blog = await getBlogData(params.slug);
-  if (!blog) {
-    // TODO: update error handling
-    return <h1>Something went wrong</h1>;
-  }
 
   return (
     <div className="min-h-[80vh] h-full w-full bg-gray-50 max-w-4xl mx-auto my-28">
-      <div className="mx-6 md:mx-0">
-        <h1 className="text-4xl font-medium">{blog.title}</h1>
-        <div className="mt-1 flex items-center gap-x-2 text-gray-500">
-          <p>{formatDate(blog.createdAt)}</p>
-          <Dots bgColor="bg-gray-500" />
-          <p>{blog.timeToRead} min read</p>
-        </div>
-        <div className="mt-12">
-          <Image
-            src={`/images/blogs/${params.slug}/cover/resource.jpg`}
-            alt={blog.cover?.alt ?? ''}
-            width={700}
-            height={500}
-            className="rounded-md w-full"
-            priority
-          />
-          <div className="flex justify-center mt-2">
-            <span>
-              Photo by{' '}
-              <a
-                href={blog.cover?.ownerLink ?? ''}
-                target="_blank"
-                className="underline"
-              >
-                {blog.cover?.owner}
-              </a>
-            </span>
+      {blog ? (
+        <div className="mx-6 md:mx-0">
+          <h1 className="text-4xl font-medium">{blog.title}</h1>
+          <div className="mt-1 flex items-center gap-x-2 text-gray-500">
+            <p>{formatDate(blog.createdAt)}</p>
+            <Dots bgColor="bg-gray-500" />
+            <p>{blog.timeToRead} min read</p>
           </div>
-        </div>
-        <main className="w-full mt-20 prose max-w-none break-word-blog-content">
-          {blog.content ? (
-            <DocumentRenderer
-              document={await blog.content()}
-              componentBlocks={{
-                youtubeEmbed: (props) => (
-                  <YouTubeEmbed youtubeLink={props.youtubeLink} />
-                ),
-                twitterEmbed: (props) => {
-                  const tweetId = props.tweet
-                    .split('/status/')[1]
-                    .split('?')[0];
-
-                  return <TwitterEmbed tweetId={tweetId} />;
-                },
-                image: (props) => {
-                  return (
-                    <Image
-                      src={props.src}
-                      alt={props.alt}
-                      width={props.width}
-                      height={props.height}
-                      className="w-auto h-auto mx-auto"
-                      priority
-                    />
-                  );
-                },
-              }}
+          <div className="mt-12">
+            <Image
+              src={`/images/blogs/${params.slug}/cover/resource.jpg`}
+              alt={blog.cover?.alt ?? ''}
+              width={700}
+              height={500}
+              className="rounded-md w-full"
+              priority
             />
-          ) : null}
-        </main>
-      </div>
+            <div className="flex justify-center mt-2">
+              <span>
+                Photo by{' '}
+                <a
+                  href={blog.cover?.ownerLink ?? ''}
+                  target="_blank"
+                  className="underline"
+                >
+                  {blog.cover?.owner}
+                </a>
+              </span>
+            </div>
+          </div>
+          <main className="w-full mt-20 prose max-w-none break-word-blog-content">
+            {blog.content ? (
+              <DocumentRenderer
+                document={await blog.content()}
+                componentBlocks={{
+                  youtubeEmbed: (props) => (
+                    <YouTubeEmbed youtubeLink={props.youtubeLink} />
+                  ),
+                  twitterEmbed: (props) => {
+                    const tweetId = props.tweet
+                      .split('/status/')[1]
+                      .split('?')[0];
+
+                    return <TwitterEmbed tweetId={tweetId} />;
+                  },
+                  image: (props) => {
+                    return (
+                      <Image
+                        src={props.src}
+                        alt={props.alt}
+                        width={props.width}
+                        height={props.height}
+                        className="w-auto h-auto mx-auto"
+                        priority
+                      />
+                    );
+                  },
+                }}
+              />
+            ) : null}
+          </main>
+        </div>
+      ) : (
+        <h1>Something went wrong</h1>
+      )}
     </div>
   );
+}
+
+export async function generateStaticParams() {
+  const slugs = await getBlogSlugs();
+
+  return slugs.map((slug) => ({
+    slug,
+  }));
 }
