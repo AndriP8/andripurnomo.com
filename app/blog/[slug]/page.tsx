6bbab2dbea00b1f34a3ec8ed18422a8f575ecea7
Andri Purnomo <andri.adrp@gmail.com>
2023-12-31 08:49:54 +0700

    fix: test are homepage can reveal blog content

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -34,74 +34,74 @@ type PageProps = {
 
 export default async function Page({ params }: PageProps) {
   const blog = await getBlogData(params.slug);
-  if (blog) {
-    return (
-      <div className="min-h-[80vh] h-full w-full bg-gray-50 max-w-4xl mx-auto my-28">
-        <div className="mx-6 md:mx-0">
-          <h1 className="text-4xl font-medium">{blog.title}</h1>
-          <div className="mt-1 flex items-center gap-x-2 text-gray-500">
-            <p>{formatDate(blog.createdAt)}</p>
-            <Dots bgColor="bg-gray-500" />
-            <p>{blog.timeToRead} min read</p>
-          </div>
-          <div className="mt-12">
-            <Image
-              src={`/images/blogs/${params.slug}/cover/resource.jpg`}
-              alt={blog.cover?.alt ?? ''}
-              width={700}
-              height={500}
-              className="rounded-md w-full"
-              priority
-            />
-            <div className="flex justify-center mt-2">
-              <span>
-                Photo by{' '}
-                <a
-                  href={blog.cover?.ownerLink ?? ''}
-                  target="_blank"
-                  className="underline"
-                >
-                  {blog.cover?.owner}
-                </a>
-              </span>
-            </div>
-          </div>
-          <main className="w-full mt-20 prose max-w-none break-word-blog-content">
-            {blog.content ? (
-              <DocumentRenderer
-                document={await blog.content()}
-                componentBlocks={{
-                  youtubeEmbed: (props) => (
-                    <YouTubeEmbed youtubeLink={props.youtubeLink} />
-                  ),
-                  twitterEmbed: (props) => {
-                    const tweetId = props.tweet
-                      .split('/status/')[1]
-                      .split('?')[0];
-
-                    return <TwitterEmbed tweetId={tweetId} />;
-                  },
-                  image: (props) => {
-                    return (
-                      <Image
-                        src={props.src}
-                        alt={props.alt}
-                        width={props.width}
-                        height={props.height}
-                        className="w-auto h-auto mx-auto"
-                        priority
-                      />
-                    );
-                  },
-                }}
-              />
-            ) : null}
-          </main>
-        </div>
-      </div>
-    );
-  } else {
+  if (!blog) {
     // TODO: update error handling
     return <h1>Something went wrong</h1>;
   }
+
+  return (
+    <div className="min-h-[80vh] h-full w-full bg-gray-50 max-w-4xl mx-auto my-28">
+      <div className="mx-6 md:mx-0">
+        <h1 className="text-4xl font-medium">{blog.title}</h1>
+        <div className="mt-1 flex items-center gap-x-2 text-gray-500">
+          <p>{formatDate(blog.createdAt)}</p>
+          <Dots bgColor="bg-gray-500" />
+          <p>{blog.timeToRead} min read</p>
+        </div>
+        <div className="mt-12">
+          <Image
+            src={`/images/blogs/${params.slug}/cover/resource.jpg`}
+            alt={blog.cover?.alt ?? ''}
+            width={700}
+            height={500}
+            className="rounded-md w-full"
+            priority
+          />
+          <div className="flex justify-center mt-2">
+            <span>
+              Photo by{' '}
+              <a
+                href={blog.cover?.ownerLink ?? ''}
+                target="_blank"
+                className="underline"
+              >
+                {blog.cover?.owner}
+              </a>
+            </span>
+          </div>
+        </div>
+        <main className="w-full mt-20 prose max-w-none break-word-blog-content">
+          {blog.content ? (
+            <DocumentRenderer
+              document={await blog.content()}
+              componentBlocks={{
+                youtubeEmbed: (props) => (
+                  <YouTubeEmbed youtubeLink={props.youtubeLink} />
+                ),
+                twitterEmbed: (props) => {
+                  const tweetId = props.tweet
+                    .split('/status/')[1]
+                    .split('?')[0];
+
+                  return <TwitterEmbed tweetId={tweetId} />;
+                },
+                image: (props) => {
+                  return (
+                    <Image
+                      src={props.src}
+                      alt={props.alt}
+                      width={props.width}
+                      height={props.height}
+                      className="w-auto h-auto mx-auto"
+                      priority
+                    />
+                  );
+                },
+              }}
+            />
+          ) : null}
+        </main>
+      </div>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import { getBlogs } from '@lib/data';
+import { DocumentRenderer } from '@keystatic/core/renderer';
+import { getBlogData, getBlogs } from '@lib/data';
+import { formatDate } from '@lib/utils';
 import { Footer, Navbar } from '@ui/components';
 import { CardLinkBlog } from '@ui/components';
+import { TwitterEmbed, YouTubeEmbed } from '@ui/components';
+import { Dots } from '@ui/components/Dots';
 import { Metadata } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export const metadata: Metadata = {
@@ -10,6 +15,11 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   const blogs = await getBlogs(3);
+  const blog = await getBlogData('setup-react-from-scratch');
+  if (!blog) {
+    // TODO: update error handling
+    return <h1>Something went wrong</h1>;
+  }
 
   return (
     <>
@@ -47,6 +57,69 @@ export default async function Page() {
         </div>
       </main>
       <Footer />
+      <div className="min-h-[80vh] h-full w-full bg-gray-50 max-w-4xl mx-auto my-28">
+        <div className="mx-6 md:mx-0">
+          <h1 className="text-4xl font-medium">{blog.title}</h1>
+          <div className="mt-1 flex items-center gap-x-2 text-gray-500">
+            <p>{formatDate(blog.createdAt)}</p>
+            <Dots bgColor="bg-gray-500" />
+            <p>{blog.timeToRead} min read</p>
+          </div>
+          <div className="mt-12">
+            <Image
+              src={`/images/blogs/${'setup-react-from-scratch'}/cover/resource.jpg`}
+              alt={blog.cover?.alt ?? ''}
+              width={700}
+              height={500}
+              className="rounded-md w-full"
+              priority
+            />
+            <div className="flex justify-center mt-2">
+              <span>
+                Photo by{' '}
+                <a
+                  href={blog.cover?.ownerLink ?? ''}
+                  target="_blank"
+                  className="underline"
+                >
+                  {blog.cover?.owner}
+                </a>
+              </span>
+            </div>
+          </div>
+          <main className="w-full mt-20 prose max-w-none break-word-blog-content">
+            {blog.content ? (
+              <DocumentRenderer
+                document={await blog.content()}
+                componentBlocks={{
+                  youtubeEmbed: (props) => (
+                    <YouTubeEmbed youtubeLink={props.youtubeLink} />
+                  ),
+                  twitterEmbed: (props) => {
+                    const tweetId = props.tweet
+                      .split('/status/')[1]
+                      .split('?')[0];
+
+                    return <TwitterEmbed tweetId={tweetId} />;
+                  },
+                  image: (props) => {
+                    return (
+                      <Image
+                        src={props.src}
+                        alt={props.alt}
+                        width={props.width}
+                        height={props.height}
+                        className="w-auto h-auto mx-auto"
+                        priority
+                      />
+                    );
+                  },
+                }}
+              />
+            ) : null}
+          </main>
+        </div>
+      </div>
     </>
   );
 }
