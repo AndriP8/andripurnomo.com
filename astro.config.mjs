eefc8b9dbb3d523da384399269d3f86d0666183f
Andri Purnomo <andri.adrp@gmail.com>
2026-09-01 09:48:02 +0700

    build(config): add markdown-remark integration and vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,20 @@ import sitemap from '@astrojs/sitemap';
 import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import { unified } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.andripurnomo.com',
-  integrations: [
-    tailwind(),
-    mdx({
+  markdown: {
+    processor: unified({
       remarkPlugins: [remarkGfm],
       rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
     }),
+  },
+  integrations: [
+    tailwind(),
+    mdx(),
     react(),
     sitemap(),
   ],
